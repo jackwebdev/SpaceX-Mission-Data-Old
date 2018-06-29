6493a625683f25ca11782feb9805adfffe95015a
Jck1997 <Jcke1997@gmail.com>
2018-06-29 11:34:15 +0100

    Updated name conventions for variables 🚀

--- js/app.js
+++ js/app.js
@@ -6,22 +6,22 @@ let latestLaunchData;
 let pastLaunchData;
 let type;
 
-let MissionLogo;
-let MissionNoLogo = 'https://media.wired.com/photos/5a7cb68fa2d3835392e1b469/1:1/w_1000,c_limit/spacexrocketreturn.jpg'
-let Details;
-let FlightNumber;
-let MissionName;
-let RocketName;
-let LaunchDate;
-let LaunchLoc;
-let LandSuccess;
-let Success;
-let VideoLink;
-let Presskit;
-let PayloadType;
-let PayloadCustomer;
-let PayloadOrbit;
-let PayloadKG;       
+let missionLogo;
+let missionNoLogo = 'https://media.wired.com/photos/5a7cb68fa2d3835392e1b469/1:1/w_1000,c_limit/spacexrocketreturn.jpg'
+let details;
+let flightNumber;
+let missionName;
+let rocketName;
+let launchDate;
+let launchLoc;
+let landsuccess;
+let success;
+let videoLink;
+let pressKit;
+let payloadType;
+let payloadCustomer;
+let payloadOrbit;
+let payloadKG;       
 
 //Set the basic structure for the HTML
 const rootDiv = document.getElementById('root');
@@ -77,25 +77,25 @@ function upcomingLaunch() {
     //Upcoming data entries
         //Assign the data into variables
         if (upcomingLaunchData !== undefined) {
-            MissionLogo = upcomingLaunchData[0]['links']['mission_patch'];
-            MissionNoLogo = 'https://media.wired.com/photos/5a7cb68fa2d3835392e1b469/1:1/w_1000,c_limit/spacexrocketreturn.jpg'
-            Details =  upcomingLaunchData[0]['details'];
-            FlightNumber = upcomingLaunchData[0]['flight_number'];
-            MissionName = upcomingLaunchData[0]['mission_name'];
-            RocketName = upcomingLaunchData[0]['rocket']['rocket_name'];
-            LaunchDate = upcomingLaunchData[0]['launch_date_unix'];
-            LaunchLoc = upcomingLaunchData[0]['launch_site']['site_name_long'];
-            LandSuccess = upcomingLaunchData[0]['rocket']['first_stage']['cores'][0]['land_success'];
-            Success = upcomingLaunchData[0]['launch_success'];
-            VideoLink = upcomingLaunchData[0]['links']['video_link'];
-            Presskit = upcomingLaunchData[0]['links']['presskit'];
-            PayloadType = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['payload_type'];
-            PayloadCustomer = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['customers'];
-            PayloadOrbit = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['orbit'];
-            PayloadKG = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['payload_mass_kg'];         
+            missionLogo = upcomingLaunchData[0]['links']['mission_patch'];
+            missionNoLogo = 'https://media.wired.com/photos/5a7cb68fa2d3835392e1b469/1:1/w_1000,c_limit/spacexrocketreturn.jpg'
+            details =  upcomingLaunchData[0]['details'];
+            flightNumber = upcomingLaunchData[0]['flight_number'];
+            missionName = upcomingLaunchData[0]['mission_name'];
+            rocketName = upcomingLaunchData[0]['rocket']['rocket_name'];
+            launchDate = upcomingLaunchData[0]['launch_date_unix'];
+            launchLoc = upcomingLaunchData[0]['launch_site']['site_name_long'];
+            landsuccess = upcomingLaunchData[0]['rocket']['first_stage']['cores'][0]['land_success'];
+            success = upcomingLaunchData[0]['launch_success'];
+            videoLink = upcomingLaunchData[0]['links']['video_link'];
+            pressKit = upcomingLaunchData[0]['links']['pressKit'];
+            payloadType = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['payload_type'];
+            payloadCustomer = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['customers'];
+            payloadOrbit = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['orbit'];
+            payloadKG = upcomingLaunchData[0]['rocket']['second_stage']['payloads'][0]['payload_mass_kg'];         
         
             // Time Converter
-            gbTime = timeConverter(LaunchDate);
+            gbTime = timeConverter(launchDate);
             
             //Generate HTML Card
             type = 'Upcoming';
@@ -139,24 +139,24 @@ function latestLaunch() {
     //latest data entries
         //Assign the data into variables
         if (latestLaunchData !== undefined) {
-            MissionLogo = latestLaunchData['links']['mission_patch'];  
-            Details = latestLaunchData['details'];;
-            FlightNumber = latestLaunchData['flight_number'];
-            MissionName = latestLaunchData['mission_name'];
-            RocketName = latestLaunchData['rocket']['rocket_name'];
-            LaunchDate = latestLaunchData['launch_date_unix'];
-            LaunchLoc = latestLaunchData['launch_site']['site_name_long'];
-            LandSuccess = latestLaunchData['rocket']['first_stage']['cores'][0]['land_success'];
-            Success = latestLaunchData['launch_success'];
-            VideoLink = latestLaunchData['links']['video_link'];
-            Presskit = latestLaunchData['links']['presskit'];
-            PayloadType = latestLaunchData['rocket']['second_stage']['payloads'][0]['payload_type'];
-            PayloadCustomer = latestLaunchData['rocket']['second_stage']['payloads'][0]['customers'];
-            PayloadOrbit = latestLaunchData['rocket']['second_stage']['payloads'][0]['orbit'];
-            PayloadKG = latestLaunchData['rocket']['second_stage']['payloads'][0]['payload_mass_kg'];
+            missionLogo = latestLaunchData['links']['mission_patch'];  
+            details = latestLaunchData['details'];;
+            flightNumber = latestLaunchData['flight_number'];
+            missionName = latestLaunchData['mission_name'];
+            rocketName = latestLaunchData['rocket']['rocket_name'];
+            launchDate = latestLaunchData['launch_date_unix'];
+            launchLoc = latestLaunchData['launch_site']['site_name_long'];
+            landsuccess = latestLaunchData['rocket']['first_stage']['cores'][0]['land_success'];
+            success = latestLaunchData['launch_success'];
+            videoLink = latestLaunchData['links']['video_link'];
+            pressKit = latestLaunchData['links']['pressKit'];
+            payloadType = latestLaunchData['rocket']['second_stage']['payloads'][0]['payload_type'];
+            payloadCustomer = latestLaunchData['rocket']['second_stage']['payloads'][0]['customers'];
+            payloadOrbit = latestLaunchData['rocket']['second_stage']['payloads'][0]['orbit'];
+            payloadKG = latestLaunchData['rocket']['second_stage']['payloads'][0]['payload_mass_kg'];
                   
             // Time Converter
-            gbTime = timeConverter(LaunchDate);
+            gbTime = timeConverter(launchDate);
             
             // Generate the HTML box
             type = 'Latest';
@@ -199,24 +199,24 @@ function pastLaunch() {
     // Loop through the past data in reverse and skip the latest mission.
     if (pastLaunchData !== undefined) {
         for(let i = pastLaunchData.length -2; i >= 0; i--) {
-            MissionLogo = pastLaunchData[i]['links']['mission_patch_small'];  
-            Details = pastLaunchData[i]['details'];;
-            FlightNumber = pastLaunchData[i]['flight_number'];
-            MissionName = pastLaunchData[i]['mission_name'];
-            RocketName = pastLaunchData[i]['rocket']['rocket_name'];
-            LaunchDate = pastLaunchData[i]['launch_date_unix'];
-            LaunchLoc = pastLaunchData[i]['launch_site']['site_name_long'];
-            LandSuccess = pastLaunchData[i]['rocket']['first_stage']['cores'][0]['land_success'];
-            Success = pastLaunchData[i]['launch_success'];
-            VideoLink = pastLaunchData[i]['links']['video_link'];
-            Presskit = pastLaunchData[i]['links']['presskit'];
-            PayloadType = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['payload_type'];
-            PayloadCustomer = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['customers'];
-            PayloadOrbit = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['orbit'];
-            PayloadKG = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['payload_mass_kg'];
+            missionLogo = pastLaunchData[i]['links']['mission_patch_small'];  
+            details = pastLaunchData[i]['details'];;
+            flightNumber = pastLaunchData[i]['flight_number'];
+            missionName = pastLaunchData[i]['mission_name'];
+            rocketName = pastLaunchData[i]['rocket']['rocket_name'];
+            launchDate = pastLaunchData[i]['launch_date_unix'];
+            launchLoc = pastLaunchData[i]['launch_site']['site_name_long'];
+            landsuccess = pastLaunchData[i]['rocket']['first_stage']['cores'][0]['land_success'];
+            success = pastLaunchData[i]['launch_success'];
+            videoLink = pastLaunchData[i]['links']['video_link'];
+            pressKit = pastLaunchData[i]['links']['pressKit'];
+            payloadType = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['payload_type'];
+            payloadCustomer = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['customers'];
+            payloadOrbit = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['orbit'];
+            payloadKG = pastLaunchData[i]['rocket']['second_stage']['payloads'][0]['payload_mass_kg'];
             
             // Time Converter
-            gbTime = timeConverter(LaunchDate);
+            gbTime = timeConverter(launchDate);
          
             // Generate the HTML box
                type = 'Past';
@@ -244,14 +244,15 @@ function generateHtmlCard(type){
         card.setAttribute('class', 'grid-item');
 
         const h3 = document.createElement('h3');
-        h3.textContent = ` ${type} - ${MissionName}`;
+        h3.textContent = ` ${type} - ${missionName}`;
+        h3.setAttribute('id', missionName);
 
         const img = document.createElement('img');
-        if (MissionLogo !== null) {
-            img.src = `${MissionLogo}`;
+        if (missionLogo !== null) {
+            img.src = `${missionLogo}`;
             img.className = `mission-badge`;
         } else {
-            img.src = `${MissionNoLogo}`;
+            img.src = `${missionNoLogo}`;
             img.className = `no-mission-badge`
         }
 
@@ -260,48 +261,53 @@ function generateHtmlCard(type){
     
         //Check the kind of card. If it's past or latest and has the details show details, else show the upcoming time and date.
         if(type === 'Latest' || type === 'Past'){
-            if(Details !== null){
-                h4.textContent = `${Details}`;
+            if(details !== null){
+                h4.textContent = `${details}`;
                 } else {
-                    h4.innerHTML = `Flight No: ${FlightNumber} launched the ${RocketName} on <strong>${dateStringGB}</strong> at <strong>${time12Hour}</strong>`;
+                    h4.innerHTML = `Flight No: ${flightNumber} launched the ${rocketName} on <strong>${dateStringGB}</strong> at <strong>${time12Hour}</strong>`;
                 }
         } else {
-            h4.innerHTML += `<strong>${MissionName}</strong> will use the <strong>${RocketName}</strong> and launch on <strong>${dateStringGB}</strong> at <strong>${time12Hour}</strong>`;
+            h4.innerHTML += `<strong>${missionName}</strong> will use the <strong>${rocketName}</strong> and launch on <strong>${dateStringGB}</strong> at <strong>${time12Hour}</strong>`;
         }
     
         const ul = document.createElement('ul');
         
         const li = document.createElement('ul');
-        ul.innerHTML += `<li><strong>Rocket:</strong> ${RocketName}</li>`;
-        ul.innerHTML += `<li><strong>Flight Number:</strong> ${FlightNumber}</li>`;
+
+        ul.innerHTML += `<li><strong>Rocket:</strong> ${rocketName}</li>`;
+        ul.innerHTML += `<li><strong>Flight Number:</strong> ${flightNumber}</li>`;
         ul.innerHTML += `<li><strong>Launch Date:</strong> ${gbTime}</li>`;
-        ul.innerHTML += `<li><strong>Location:</strong> ${LaunchLoc}</li>`;
-        ul.innerHTML += `<li><strong>Payload Type:</strong> ${PayloadType}</li>`;
-        ul.innerHTML += `<li><strong>Customer:</strong> ${PayloadCustomer}</li>`;
-        ul.innerHTML += `<li><strong>Orbit:</strong> ${PayloadOrbit}</li>`;
-        if(PayloadKG !== null){ 
-         ul.innerHTML += `<li><strong>Payload Mass:</strong> ${PayloadKG} kg</li>`;
+        ul.innerHTML += `<li><strong>Location:</strong> ${launchLoc}</li>`;
+        ul.innerHTML += `<li><strong>Payload Type:</strong> ${payloadType}</li>`;
+        ul.innerHTML += `<li><strong>Customer:</strong> ${payloadCustomer}</li>`;
+        ul.innerHTML += `<li><strong>Orbit:</strong> ${payloadOrbit}</li>`;
+
+        if(payloadKG !== null){ 
+         ul.innerHTML += `<li><strong>Payload Mass:</strong> ${payloadKG} kg</li>`;
         }
         
-        if(LandSuccess !== null) {    
-            ul.innerHTML += `<li><strong>Land Success:</strong> ${LandSuccess}</li>`;
-        };
-        
-        if(Success !== null) { 
-            ul.innerHTML += `<li><strong>Launch Success:</strong> ${Success}</li>`;
-
-        };
-        
-        if(VideoLink !== null) { 
-        ul.innerHTML += `<li><a class="btn" href="${VideoLink}" target="_blank"><button>WATCH STREAM</button></a></li>`;
+        if(landsuccess !== null) {    
+            ul.innerHTML += `<li><strong>Land success:</strong> ${landsuccess}</li>`;
+        } else if (landsuccess === false) {
+            ul.innerHTML += `<li><strong>Land success:</strong> ${landsuccess}</li>`;
         } else {
-            ul.innerHTML += `<li><a class="btn"><button>More soon...</button></a></li>`;
+            ul.innerHTML += `<li><strong>Land success:</strong> No landing needed</li>`;
+        }
+        
+        if(success !== null) { 
+            ul.innerHTML += `<li><strong>Launch success:</strong> ${success}</li>`;
 
+        };
+        
+        if(videoLink !== null) { 
+         ul.innerHTML += `<li><a class="btn" href="${videoLink}" target="_blank"><button>WATCH STREAM</button></a></li>`;
+        } else {
+         ul.innerHTML += `<li><a class="btn"><button>More soon...</button></a></li>`;
         }
 
-        if (Presskit !== null) {
-            ul.innerHTML += `<li><a class="btn" href="${Presskit}" target="_blank"><button>READ PDF</button></a></li>`;
-    }
+        if (pressKit !== null) {
+            ul.innerHTML += `<li><a class="btn" href="${pressKit}" target="_blank"><button>READ PDF</button></a></li>`;
+        }
 
     container.appendChild(card);
     card.appendChild(h3);
